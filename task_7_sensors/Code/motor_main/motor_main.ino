f2025875d1000413d5339869bc8c2b3f79c968a5
Aaron <aaaaaaronchong@gmail.com>
2017-10-08 14:43:06 -0400

    library added

--- task_7_sensors/Code/motor_main/motor_main.ino
+++ task_7_sensors/Code/motor_main/motor_main.ino
@@ -1,13 +1,26 @@
-#include <digitalWriteFast.h> //thank you jrraines!
+#include "dcMotor.h"
+//#include <digitalWriteFast.h> //thank you jrraines!
 //https://code.google.com/archive/p/digitalwritefast/downloads
 
+dcMotor dc_test(3, 4, 5);
 
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  dc_test.speed(50);
+  dc_test.cw();
+  delay(5000);
+  dc_test.speed(80);
+  delay(5000);
+  dc_test.speed(50);
+  delay(5000);
+  dc_test.ccw();
+  dc_test.speed(80);
+  delay(5000);
+  dc_test.speed(0);
+  delay(10000);
 }
