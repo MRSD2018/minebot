aada746da6dc94ddec50e69e278e0407c6e3ee60
Aaron <aaaaaaronchong@gmail.com>
2017-10-08 15:18:37 -0400

    finished motor lib

--- task_7_sensors/Code/motor_main/motor_main.ino
+++ task_7_sensors/Code/motor_main/motor_main.ino
@@ -2,7 +2,7 @@
 //#include <digitalWriteFast.h> //thank you jrraines!
 //https://code.google.com/archive/p/digitalwritefast/downloads
 
-dcMotor dc_test(3, 4, 5, 2, 6);
+dcMotor dc_test(3, 4, 5);
 
 void setup() {
   // put your setup code here, to run once:
@@ -11,16 +11,15 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  dc_test.speed(50);
-  dc_test.cw();
+  dc_test.fullControl(50);
   delay(5000);
-  dc_test.speed(80);
+  dc_test.fullControl(-50);
   delay(5000);
-  dc_test.speed(50);
+  dc_test.fullControl(80);
   delay(5000);
   dc_test.ccw();
-  dc_test.speed(80);
+  dc_test.fullControl(-80);
   delay(5000);
-  dc_test.speed(0);
+  dc_test.fullControl(0);
   delay(10000);
 }
