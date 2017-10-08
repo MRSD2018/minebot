c1043222431e84e0e7d1e5828bee58434b36332a
U-DESKTOP-90JS58P\aaron <aaaaaaronchong@gmail.com>
2017-10-08 14:02:32 -0400

    nothing much

---
+++ task_7_sensors/Code/motor_main/motor_main.ino
@@ -0,0 +1,13 @@
+#include <digitalWriteFast.h> //thank you jrraines!
+//https://code.google.com/archive/p/digitalwritefast/downloads
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
