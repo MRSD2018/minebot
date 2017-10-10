dcf41de5203df72625c039ebf6cdd6fcaca56e51
Edward Terry <edward.william.terry@gmail.com>
2017-10-09 23:27:34 -0400

    tuned PID for DC motor position control

--- task_7_sensors/Code/motor_pos_control/motor_pos_control.ino
+++ task_7_sensors/Code/motor_pos_control/motor_pos_control.ino
@@ -1,37 +1,41 @@
-#include <digitalWriteFast.h> //thank you jrraines!! 
+#include "digitalWriteFast.h" //thank you jrraines!! 
+#include "dcMotor.h"
 //https://code.google.com/archive/p/digitalwritefast/downloads
 
 #define PWM 6
-#define L1 4        //L1 high L2 low counter clockwise, L1 low L2 high clockwise
+#define L1 4
 #define L2 5
 #define channelAPin 3
 #define channelBPin 7
-#define holesPerRevo 180  //how many holes are there in the encoder for 1 motor output turn
-#define resPerCycle 2 //if i read only RISING then it will be 1, CHANGE means both RISING and FALLING so = 2
+#define holesPerRev 180  //how many holes are there in the encoder for 1 motor output turn
+#define revPerCycle 2 //if i read only RISING then it will be 1, CHANGE means both RISING and FALLING so = 2 
+
+dcMotor motor(PWM, L1, L2);
 
 volatile bool channelAVal;    //binary high or low from encoder
 volatile bool channelBVal;
 volatile long encoderTicks = 0;   //a tick is a change in channel A, both RISING and FALLING edges
 
-long past = 0;   //time in microseconds
-long now = 0;
+float commandedPositionDeg;
+float prevActualPositionDeg;
+float prevPositionError;
 
-double revo = 0;
-double pastRevo = 0;
-double rpm = 0;
+double kp = 7;
+double kd = 250;
+double ki = 0.00018;
 
-double kp = 0.1;
-double kd = 0.1;
-double ki = 0;
+unsigned long prevTime;
+float positionErrorSum;
+float motorInputScaled;
 
-String serialstuff;
-long rpmDesired = 40;
-long pwmToWrite = 0;
+unsigned long nowTime;
+double dt;
 
-long pos = 0;
-long posPast = 0;
-long goalPos = 0;
-long deg = 0;
+float actualPositionDeg;
+float positionError;
+
+int pwmToWrite;
+String serialInput;
 
 void setup() {
   Serial.begin(9600);
@@ -39,86 +43,78 @@ void setup() {
   pinMode(L2, OUTPUT);
   pinMode(PWM, OUTPUT);
 
-  pinMode(channelAPin, INPUT); //should I do internal pullup?
+  pinMode(channelAPin, INPUT);
   pinMode(channelBPin, INPUT);
-  attachInterrupt(digitalPinToInterrupt(channelAPin), handleChannelA, CHANGE);   //attaching the callback
+  attachInterrupt(digitalPinToInterrupt(channelAPin), encoderCount, CHANGE);
 }
 
 void loop() {
+  // calculate current time and timestep
+  nowTime = millis();
+  dt = double(nowTime - prevTime); // note this is in milliseconds still
 
-  while(Serial.available())
-  {
-    serialstuff = Serial.readString();
-    deg = serialstuff.toInt();
-    Serial.print(deg);
+  // parse serial input
+  while (Serial.available()) {
+    serialInput = Serial.readString();
+    commandedPositionDeg = serialInput.toInt();
   }
 
-//Position Control
-  revo = (double)encoderTicks/(holesPerRevo * resPerCycle);  //revolutions in float
-  now = micros();
-  pos = posPast + ((revo-pastRevo) * 360) ; //finding position in deg
+  // get current position
+  actualPositionDeg = encoderTicks * (double)holesPerRev * revPerCycle / 360; // 360 degrees in revolution
 
-//PD control
-  goalPos = (kd*((double)(deg - pos))) + (kp*(double)(revo-pastRevo)*(360/(now-past/1000000)));
-  pwmToWrite = abs(goalPos*100); //Set motor speed
-  if(pwmToWrite>255){pwmToWrite = 255;} //Limit max PWM
-  if(pwmToWrite<255){pwmToWrite = 0;} //Limit min PWM
-  
-  Serial.println(goalPos);
+  // calculate errors
+  positionError = commandedPositionDeg - actualPositionDeg;
+  positionErrorSum += positionError * dt;
 
-  //Motor CCW
-  if(goalPos<0){
-    ccw();
-    analogWrite(PWM, pwmToWrite);
+  // calculate PWM
+  pwmToWrite =  kp * positionError
+                + kd * (positionError - prevPositionError) / dt
+                + ki * (positionErrorSum);
+  motorInputScaled = (double)pwmToWrite / 255 * 100;
+
+  // drive motor
+  if (motorInputScaled > 100) {
+    motorInputScaled = 100;
+  } else if (motorInputScaled < -100) {
+    motorInputScaled = -100;
   }
-  //Motor CW
-  if(goalPos>0) {
-    cw();
-    analogWrite(PWM, pwmToWrite);
-  }
+  motor.fullControl(motorInputScaled);
 
-  //Update
-  pastRevo = revo;
-  posPast = pos;
-  past = now;
-  
-  //calculate the revolutions
-//  revo = (double)encoderTicks/(holesPerRevo * resPerCycle);  //revolutions in float
-//
-//  now = micros();
-//  rpm = (revo - pastRevo)*60/((double)(now-past)/1000000);  //finding the rpm
-//  pastRevo = revo;
-//  past = now;
-//
-//  pwmToWrite += (kp*((double)rpmDesired-rpm));
-//  if(pwmToWrite>255){pwmToWrite = 255;}
-//  else if(pwmToWrite<0){pwmToWrite = 0;}
+  // update values for next timestep
+  prevPositionError = positionError;
+  prevTime = nowTime;
 
-//  analogWrite(PWM,pwmToWrite);  //analogWrite only allows 0 to 255 on UNO
-//  analogWrite(PWM,100);
-//  Serial.print(pwmToWrite);
-//  Serial.print("\t");
-//  Serial.println(rpm);
+  // print to serial monitor
+  Serial.print("Encoder counts: ");
+  Serial.print(encoderTicks);
+  Serial.print("Commanded position: ");
+  Serial.print(commandedPositionDeg);
+  Serial.print("  Position error: ");
+  Serial.println(positionError);
 }
 
-void handleChannelA()
+void encoderCount()
 {
   channelAVal = digitalReadFast(channelAPin);
   channelBVal = digitalReadFast(channelBPin);
-  if(channelAVal == channelBVal){encoderTicks--;} 
-  else{encoderTicks++;}//cw as positive
+  if (channelAVal == channelBVal) {
+    encoderTicks++;
+  }
+  else {
+    encoderTicks--;
+  }
 }
 
 void ccw()
 {
-  digitalWriteFast(L1,HIGH);
-  digitalWriteFast(L2,LOW);
+  digitalWriteFast(L1, HIGH);
+  digitalWriteFast(L2, LOW);
 }
 
 void cw()
 {
-  digitalWriteFast(L1,LOW);
-  digitalWriteFast(L2,HIGH);
+  digitalWriteFast(L1, LOW);
+  digitalWriteFast(L2, HIGH);
 }
 
 
