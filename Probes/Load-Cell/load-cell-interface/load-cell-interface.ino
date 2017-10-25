ab6bcb5c40fa65ad742e67c304f8a9c877784cfd
David Robinson <dvsrobinson@gmail.com>
2017-10-25 15:57:34 -0400

    initial commit

---
+++ Probes/Load-Cell/load-cell-interface/load-cell-interface.ino
@@ -0,0 +1,23 @@
+// Code for interfacing with Load Cell
+
+#include "HX711.h"
+
+#define DOUT  3
+#define CLK  2
+
+HX711 loadCell(DOUT, CLK);
+
+void setup() {
+  Serial.begin(9600);
+//  loadCell.set_gain(32);
+  loadCell.set_scale(7000); // may need to set calibration adaptively ~ 7000
+  loadCell.set_offset(0);
+  loadCell.tare(); //Reset the scale to 0
+}
+
+void loop() {
+
+  Serial.print(loadCell.get_units(1));
+  Serial.println();
+
+}
