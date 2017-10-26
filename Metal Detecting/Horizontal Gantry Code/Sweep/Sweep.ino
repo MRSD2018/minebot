5d1d7ad706503e686ce258161461182bb6350db2
GeorgiaC <6893531+GeorgiaC@users.noreply.github.com>
2017-10-26 19:58:55 -0400

    DC Motor Gantry Update

--- Metal Detecting/Horizontal Gantry Code/Sweep/Sweep.ino
+++ Metal Detecting/Horizontal Gantry Code/Sweep/Sweep.ino
@@ -1,47 +1,72 @@
 #include <Arduino.h>
 
-#define MOTOR_STEPS 200 //how many steps per revo
-#define RPM 120
-#define MICROSTEPS 16
+#define MOTOR_STEPS 400 //how many steps per revo
+#define RPM 200
+#define MICROSTEPS 1
 
 #define DIR 10
 #define STEP 8
 #define ENABLE 11
 
-#include "DRV8834.h"
+#include "DRV8825.h"
 #define M0 10  //not sure what these are, haven't checked deeper into the libs
 #define M1 11
-DRV8834 stepper(MOTOR_STEPS, DIR, STEP, ENABLE, M0, M1);
+DRV8825 stepper(MOTOR_STEPS, DIR, STEP, ENABLE, M0, M1);
 
-//Limit Switchds
-#define B0 2
+#define BUTTON 2
 
 int desired_steps = 200;
+int dist = 2;
+volatile int button_interrupt_flag = 0;
+const int debounce_time = 20;  //ms
+
+int last_switch_time = 0;
+const int debounce_timeout = 1000; // ms
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   stepper.begin(RPM,MICROSTEPS);
-//  stepper.enable();
-  stepper.setSpeedProfile(stepper.LINEAR_SPEED, 2000, 1000);
-//  stepper.startRotate(360);
-
-  //Enable Interrupts
-  attachInterrupt(0, Limit_1, LOW); //change State when B0 input is low
-  //attachInterrupt(1, Limit_2, LOW); //change LED state when B1 input is rising
+  
+  pinMode(BUTTON, INPUT_PULLUP);
+  //stepper.setSpeedProfile(stepper.LINEAR_SPEED, 2000, 1000);
+  attachInterrupt(0, limitSwitch, RISING); //change State when B0 input is rising
 }
 
-//Switch motor direction when limit switch is contacted
-void Limit_1() {
-  RPM = -1*RPM;
+void limitSwitch(){
+  /*
+  stepper.disable();
+  dist = -1*dist;
+  button_interrupt_flag = 1;
+  Serial.println(dist);
+  */
+
+  int cur_time = millis();
+  if ( (cur_time - last_switch_time) > debounce_timeout)
+  {
+    last_switch_time = cur_time;
+    dist *= -1;
+    Serial.println(dist);
+  }
 }
 
 void loop() 
 {
-  //maybe we can dynamically change the desired_steps, then within the if caase choose direction.
-  stepper.enable();
-  stepper.setSpeedProfile(CONSTANT_SPEED, short accel=1000, short decel=1000);
-  stepper.setRPM(RPM);
+  //maybe we can dynamically change the desired_steps, then within the if case choose direction.
   
+    stepper.rotate(dist);
+    Serial.println(dist);
+  
+//  if (button_interrupt_flag) {
+//    debounce(BUTTON);
+//    button_interrupt_flag = 0;
+//  }
 }
 
-
+void debounce(int button)
+{
+  unsigned long now = millis ();
+  while (digitalRead(button) == HIGH || (millis() - now) <= debounce_time)
+  {
+    if(digitalRead(button) == HIGH){now = millis();}
+  }
+}
