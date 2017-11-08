00f6477d068b29b1f8d7b5de7b7de716b1934ee4
GeorgiaC <6893531+GeorgiaC@users.noreply.github.com>
2017-11-07 19:38:52 -0500

    Gantry Code and File Clean-up

--- Metal Detecting/Horizontal Gantry Code/Sweep-DCMotor-SabertoothDriver/Sweep-DCMotor-SabertoothDriver.ino
+++ Metal Detecting/Horizontal Gantry Code/Sweep-DCMotor-SabertoothDriver/Sweep-DCMotor-SabertoothDriver.ino
@@ -1,69 +1,96 @@
 #include "digitalWriteFast.h"
-//#include "dcMotor.h"
-//#include "ForceSensor.h"
 
-#define ANALOG 10
+//Pins
+#define Speed 10
 #define channelAPin 2
 #define channelBPin 4
-#define button 3
+#define limitSwitch 3
+#define inidcatorLED 6
 
-//dcMotor motor(PWM, L1, L2, channelAPin, channelBPin);
+//constants
+static int encoderTicksPerRotation = 48;
+static int teethPerRotation = 72;
+static int toothPitch = 2;
 
+//encoder variables
 volatile bool channelAVal;
 volatile bool channelBVal;
 volatile int encoderTicks;
-int test;
 
-
+//switch variables
+int but_interrupt_flag = 0;
 volatile int button_interrupt_flag = 0;
 const int debounce_time = 20;  //ms
 int last_switch_time = 0;
 const int debounce_timeout = 1000; // ms
+int initCounter = 0;
+volatile int limitL;
+volatile int limitR;
+
+//positioning variables
+int dist;
+int posInTicks;
+float posInCM;
 
 void setup() {
   Serial.begin(9600);
+
+  //ISR
   attachInterrupt(digitalPinToInterrupt(channelAPin), encoderCount, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(button), limitSwitch, RISING); //change State when B0 input is rising
-  pinMode(button,INPUT_PULLUP);
-  pinMode(ANALOG, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(limitSwitch), initialize, RISING); //change State when B0 input is rising
+
+  //Pin initialization
+  pinMode(limitSwitch,INPUT_PULLUP);
+  pinMode(Speed, OUTPUT);
+  pinMode(channelAPin, INPUT);
+  pinMode(channelBPin, INPUT);
+  
 }
 
 void loop() {
-  // uncomment one or the other to get position or velocity control
-  if (encoderTicks > 1800){
-    analogWrite(ANALOG, 0);
-    Serial.println("Forward");
-    
+
+  //Initialization Step
+  if (but_interrupt_flag){
+    debounce(limitSwitch);
+    if (initCounter == 1) {
+      limitL = encoderTicks;
+      Serial.print("Limit L = "); Serial.println(limitL);
+    }
+    else if (initCounter == 2) {
+      limitR = encoderTicks;
+      Serial.print("Limit R = "); Serial.println(limitR);
+    }
+    initCounter++;
+    Serial.println(initCounter);
+    but_interrupt_flag = 0;
   }
-  if (encoderTicks < 85){
-    analogWrite(ANALOG, 1023);
-    Serial.println("Backwards");
+
+  //Positioning Step
+  if (initCounter ==3) {
+    dist = abs(limitL - limitR); 
+    posInTicks = map(encoderTicks, 0, dist, limitL, limitR);
+    posInCM = posInTicks*teethPerRotation*toothPitch/encoderTicksPerRotation;
+    Serial.print(posInTicks); Serial.print(" ==>  "); Serial.println(posInCM);
   }
-  
-  Serial.println(encoderTicks);
-  delay(100);
-  
 }
 
-//Change motor direction when limit switch is hit
-void limitSwitch(){
-  //Debounce code
+//debouncing
+void debounce(int button){
   int cur_time = millis();
   if ( (cur_time - last_switch_time) > debounce_timeout)
   {
     last_switch_time = cur_time;
-    if (analogRead(ANALOG) > 512) {
-      Serial.println("Entered DEBOUCE");
-      analogWrite(ANALOG, 0);
-    }
-    else {
-      Serial.println("Entered DEBOUCE");
-      analogWrite(ANALOG, 1023);
-    }
   }
 }
 
+//Initialize the system
+void initialize() {
+  if (but_interrupt_flag == 0) {
+    but_interrupt_flag = 1;
+  }
+}
 
+//Encoder Counts
 void encoderCount()
 {
   channelAVal = digitalReadFast(channelAPin);
@@ -75,3 +102,5 @@ void encoderCount()
     encoderTicks--;
   }
 }
+
+
