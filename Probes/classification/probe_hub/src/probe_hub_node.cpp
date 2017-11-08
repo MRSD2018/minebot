063e9ed03dcfa054803d0e2e31d316a410efae34
Edward Terry <edward.william.terry@gmail.com>
2017-11-08 15:28:09 -0500

    implemented a basic simulation of probing, not yet tested

--- Probes/classification/probe_hub/src/probe_hub_node.cpp
+++ Probes/classification/probe_hub/src/probe_hub_node.cpp
@@ -1,36 +1,51 @@
 #include "ros/ros.h"
 #include "std_msgs/Bool.h"
 #include "std_msgs/Float32.h"
+
+#include <fstream>
+
 #define PI 3.14159265
 // #include <probe_hub/probe_hub_node.h>
 
 std::vector<float> motorPosVector;
-float timeBetweenProbes = 5;
+// float timeBetweenProbes = 5;
 std_msgs::Bool probe_cmd;
 double lastTime;
+float probe_angle = 0.0; // eventually increase to 30deg 
+float rad_threshold = 0.1;
 
-struct point3D
-{
-  float x, y, z;
-};
+// sample data
+std::vector<float> motorPosVector_sim;
+std::vector<double> sampleTime_sim;
+std::vector<float> gantryXPos_sim;
 
-struct point2D
-{
-  float x, y
-};
+motorPosVector_sim = [.140 .141 .145 .152];
+sampleTime_sim = [0.0 5.0 10.0 15.0];
+gantryXPos_sim = [0 .012 .024 .036];
+int sampleTime_step = 0;
 
-struct circle
-{
+struct point3D { float x, y, z; };
+
+struct point2D { float x, y };
+
+struct circle {
   point2D center;
   float rad;
 };
 
+circle mine;
+
 std::vector<point3D> contactPointsVector;
 
-void probeDataClbk(const std_msgs::Float32& msg)
+void simulatePointCapture() // send back 
 {
-  float motorPos = msg.data;
-  motorPosVector.push_back(motorPos);
+
+}
+
+void probeDataClbk(const std_msgs::Bool& msg)
+{
+  // float motorPos = msg.data;
+  // motorPosVector.push_back(motorPos);
   // for (auto it = motorPosVector.begin(); it!=motorPosVector.end(); it++)
   // {
   //   // ROS_INFO("%f",*it);
@@ -38,37 +53,54 @@ void probeDataClbk(const std_msgs::Float32& msg)
   // }
   // std::endl;
   updateContactPoints();
+  sampleTime_step++;
 }
 
-void updateContactPoints()
+void updateContactPoints() // adds to the vector of contact points, in gantry frame
 {
   contactPointsVector.push_back(contactPointToGantryFrame(motorPos));
 }
 
-void classifyShape()
+bool isMine() // determines whether collection of points represents a circle (i.e. mine)
 {
-  if (contactPointsVector.size()>=3) // below 3 points, not enough data to estimate circumcenter
+  if (contactPointsVector.size()>=3) // if fewer 3 points, there is not enough data to estimate circumcenter
   {
-    // TODO: need to project points to a common horizontal plane
-    circle circle = initCircle(std::vector<point2D>& points);
+    std::vector<point2D> projectedPoints;
+    projectedPoints.x = contactPointsVector.x;
+    projectedPoints.y = contactPointsVector.y; // simplification for planar case -> update!!
+    // TODO: need to project points to a common horizontal plane - probing angle = 0 for now so that this is not a problem
+    circle circle = calcCircle(std::vector<point2D>& projectedPoints); // 
+    if (circle.rad<rad_threshold) 
+    {
+      mine = circle; // assign the calculated circle to the mine instance;
+      return true;
+    }
+    else 
+    {
+      return false;
+    }
+
   }
+  else
+    return false;
 }
+
 
 point3D contactPointToGantryFrame(float& motorPos)
 {
-  point3D cp;
-  cp.x = getGantryXPos();
-  cp.y = motorPos*cos(30*PI/180.0)+0.3; // define these params above
-  cp.z = -motorPos*sin(30*PI/180.0)-0.4;
+  point3D cp; // cp = contact point
+  cp.x = getGantryXPos(); // currently hard coded
+  cp.y = motorPos*cos(probe_angle*PI/180.0); // define these params above
+  cp.z = 0;//-motorPos*sin(probe_angle*PI/180.0)-0.4;
   return cp;
 }
 
 float getGantryXPos() // side to side position of gantry head
 {
-  return 1; // TODO
+  return gantryXPos_sim[sampleTime_step];
 }
 
-circle initCircle(const std::vector<point2D>& points)
+circle calcCircle(const std::vector<point2D>& points)
 {
   circle circle;
   point2D cc;
@@ -78,7 +110,7 @@ circle initCircle(const std::vector<point2D>& points)
 
   int n = points.size();
 
-  for (int i = 0;i<n-2;i++){
+  for (int i = 0;i<n-2;i++){ // go through all the combinations of points
     for (int j = i+1;j<n-1;j++){
       for (int k = j+1;k<n;k++){
         // create a vector of three points
@@ -99,14 +131,12 @@ circle initCircle(const std::vector<point2D>& points)
   cc.x = sigX/q;
   cc.y = sigY/q;
   circle.center = cc;
-  circle.rad = 1; // insert calculation for this later
+  circle.rad = calcRadius(cc, points);
   return circle;
 }
 
-
 point2D circumcenter(const std::vector<point2D>& points)
 {
-
   float pIx = points[0].x;
   float pIy = points[0].y;
   float pJx = points[1].x;
@@ -135,18 +165,30 @@ point2D circumcenter(const std::vector<point2D>& points)
 
   point2D cc;
 
-  cc.x=(sqI * dJK.y + sqJ * dKI.y + sqK * dIJ.y) / (2 * det);
+  cc.x = (sqI * dJK.y + sqJ * dKI.y + sqK * dIJ.y) / (2 * det);
   cc.y = -(sqI * dJK.x + sqJ * dKI.x + sqK * dIJ.x) / (2 * det);
 
   return cc;
 }
 
+float calcRadius(point2D& cc, std::vector<point2D>& points)
+{
+  float rHat = 0;
+  float dx, dy;
+  int numPoints = points.size();
+  for (int i = 0; i<numPoints; i++){
+    dx = points[i].x - cc.x;
+    dy = points[i].y - cc.y;
+    rHat += sqrt(dx^2 + dy^2);
+  }
+  return rHat / numPoints;
+}
 
 void probeCmdLogic(double currentTime)
 {
-  if((currentTime-lastTime)>timeBetweenProbes)
+  if(currentTime>sampleTime_sim[sampleTime_step])
   {
-    lastTime = ros::Time::now().toSec();
+    // lastTime = ros::Time::now().toSec();
     probe_cmd.data = true;
   }
 }
@@ -156,7 +198,8 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "probe_hub");
   ros::NodeHandle n;
   ros::Publisher begin_probe_pub = n.advertise<std_msgs::Bool>("begin_probe", 1000);
-  ros::Subscriber probe_data_sub = n.subscribe("probe_data", 1000, probeDataClbk);
+  // ros::Subscriber probe_data_sub = n.subscribe("probe_data", 1000, probeDataClbk);
+  ros::Subscriber probe_data_returned_sub = n.subscribe("begin_probe", 1000, probeDataClbk);
   ros::Rate loop_rate(10);
 
   while (ros::ok())
@@ -165,6 +208,15 @@ int main(int argc, char **argv)
     probeCmdLogic(currentTime);
     begin_probe_pub.publish(probe_cmd);
     probe_cmd.data = false;
+
+    if(isMine)
+    {
+      ROS_INFO("Object is a mine with radius %f and center x=%f, y=%f",mine.rad,mine.center.x, mine.center.y);
+    }
+    else
+    {
+      ROS_INFO("Object is not a mine or not enough points have been sampled yet");
+    }
 
     ros::spinOnce();
 
