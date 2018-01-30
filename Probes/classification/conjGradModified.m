b4e6707a6a1a217181df0784dd37907452935154
David Robinson <dvsrobinson@gmail.com>
2018-01-30 11:29:00 -0500

    refactoring

--- Probes/classification/conjGradModified.m
+++ Probes/classification/conjGradModified.m
@@ -1,10 +1,10 @@
 clc; clear; close all;
 
-viz = true;  
+visualize = true;
 
-for loop = 1:10
+%% Perform simulation x times
+for loop = 1
 
-    %% ground truth circle
     % landmine simulation
     r = 9; % cm
     c.x = 0; c.y = 0;
@@ -12,31 +12,16 @@ for loop = 1:10
     noise = 0.2; % 0.1 is reasonable assumption
     aoa = 180; % probe angle of attack in degrees
     n = 9; % number of points
-    [ points ] = generateRandomPtsModified( r,c.x,c.y,n,aoa,noise,true );
+    [ pointsTrue, pointsFalse ] = generateRandomPtsModified( r,c.x,c.y,n,aoa,noise,true );
 
-    % get positive subset
-    counter = 1;
-    for i = 1:length(points.target)
-       if points.target(i)
-           pointsTrue.x(counter) = points.x(i);
-           pointsTrue.y(counter) = points.y(i);
-           counter = counter + 1;
-       end
-    end
-
-    % plot GT with simulated points
-    if viz
+    % plot Ground Truth with simulated points
+    if visualize
         figure; 
         hold on; axis equal; grid on;
-
-        for i = 1:length(points.target)
-            if points.target(i)
-                plot(points.x(i),points.y(i),'.green','MarkerSize',20)
-            else
-                plot(points.x(i),points.y(i),'.red','MarkerSize',20)
-            end
+        plot(pointsTrue.x,pointsTrue.y,'.green','MarkerSize',20)
+        if (~isempty(pointsFalse))
+            plot(pointsFalse.x,pointsFalse.y,'.red','MarkerSize',20)
         end
-
         plot(c.x,c.y,'.blue','MarkerSize',20)
         rectangle('Position',[c.x-r c.y-r 2*r 2*r],'Curvature',[1 1],'EdgeColor','blue')
     end
@@ -45,10 +30,10 @@ for loop = 1:10
 
     % draw circles in parameter space
     intersection = [];
-    for i = 1:length(points.x)
+    for i = 1:length(pointsTrue.x)
     %     rectangle('Position',[points.x(i)-r points.y(i)-r 2*r 2*r],'Curvature',[1 1],'EdgeColor','black')
         for j = 1:i
-            [xout,yout] = circcirc(points.x(i),points.y(i),r,points.x(j),points.y(j),r);
+            [xout,yout] = circcirc(pointsTrue.x(i),pointsTrue.y(i),r,pointsTrue.x(j),pointsTrue.y(j),r);
             for k = 1:length(xout)
                 if ( ~isnan(xout) ) 
                     intersection = [intersection; xout(k), yout(k)];
@@ -62,9 +47,9 @@ for loop = 1:10
 
     x = [];
     for i = 1:length(intersection)
-        if intersection(i,2) < max(points.y) ...
-                && intersection(i,1) < max(points.x) ...
-                && intersection(i,1) > min(points.x)
+        if intersection(i,2) < max(pointsTrue.y) ...
+                && intersection(i,1) < max(pointsTrue.x) ...
+                && intersection(i,1) > min(pointsTrue.x)
            x = [x; intersection(i,:)];
         end
     end
@@ -85,8 +70,8 @@ for loop = 1:10
     %% compute error metric
     % find line between point and circle centre
     circleFitError = 0;
-    for i = 1:length(points.x)
-        coefficients = polyfit([points.x(i), ccalc.x], [points.y(i), ccalc.y], 1);
+    for i = 1:length(pointsTrue.x)
+        coefficients = polyfit([pointsTrue.x(i), ccalc.x], [pointsTrue.y(i), ccalc.y], 1);
         slope = coefficients (1);
         intercept = coefficients (2);
 
@@ -95,15 +80,13 @@ for loop = 1:10
 
         % find closest point and computer squared error  
         for j = 1:length(xcross)
-            dist(j) = sqrt( (points.x(i)-xcross(j))^2+(points.y(i)-ycross(j))^2 ); 
+            dist(j) = sqrt( (pointsTrue.x(i)-xcross(j))^2+(pointsTrue.y(i)-ycross(j))^2 ); 
         end    
         circleFitError = circleFitError + min(dist)^2;
     end
 
-    circleFitError(loop) = circleFitError/n; % normalise by number of points becuase this is ongoing!
-%     radiusError(loop) = abs(r-rHat);
-    trueFalseRatio(loop) = length(pointsTrue) / ...
-                           length(points.target(points.target == false));
+    circleFitError(loop) = circleFitError/n; % normalized!
+    falseTrueRatio(loop) = length(pointsFalse) / length(pointsTrue);
 
 end
 
@@ -118,89 +101,4 @@ hold on; grid on;
 % histogram(radiusError,histBins)
 % title('Radius Estimate Error')
 % grid on; hold off;
-
-
-
-
-
-
-
-%% compute centre
-% [ center_x,center_y ] = initCircle( points );
-% ccalc.x = center_x; ccalc.y = center_y;
-% plot(ccalc.x,ccalc.y,'.m','MarkerSize',20)
-% rectangle('Position',[ccalc.x-r ccalc.y-r 2*r 2*r],'Curvature',[1 1],'EdgeColor','m')
-% 
-%% circle estimator w/ unknown radius
-% 
-% radiusKnown = true;
-% 
-% % eps1 = 1e-5;
-% % eps2 = 1e-5;
-% epsinner = .1;
-% epsouter = 1e-10;
-% 
-% maxIter = 100;
-% 
-% % compute rHat (eq 5)
-% if radiusKnown 
-%     rHat = r;
-% else    
-%     [ rHat ] = updateRadius( pointsTrue,ccalc );
-% end
-% % compute J (eq 6)
-% % compute gradJ (eq 7)
-% [ J,dJdx,dJdy ] = computeCost( pointsTrue,ccalc,rHat );
-% 
-% % if abs(J) < eps1 || sqrt(dJdx^2+dJdy^2)<eps2
-% %     break
-% % end
-% 
-% Jprev = J;
-% uprev = 0;
-% vprev = 0;
-% prevDJdx = dJdx; %dJdx or 0 (in code)
-% prevDJdy = dJdy; %dJdy or 0 (in code)
-% i = 1; % iterations
-% 
-% for i = 1:maxIter
-%     u = -dJdx;
-%     v = -dJdy;
-%     if i>1
-%         beta =(dJdx * (dJdx - prevDJdx) + dJdy * (dJdy - prevDJdy))/ (prevDJdx^2 + prevDJdy^2);
-%         u = u+beta*uprev;
-%         v = v+beta*vprev;
-%     end
-%     prevDJdx = dJdx;
-%     prevDJdy = dJdy;
-%     uprev    = u;
-%     vprev    = v;
-%     if i==1
-%         Jinner = J*10; % to make sure it enters inner loop
-%     end
-%     while (i<=maxIter && (abs(J-Jinner)/J)>epsinner)
-%         Jinner = J;
-%         lambda = newtonStep(u,v,rHat,pointsTrue,ccalc);
-%         ccalc.x = ccalc.x+lambda*u;
-%         ccalc.y = ccalc.y+lambda*v;
-% %         if ~radiusKnown
-%             rHat = updateRadius(pointsTrue,ccalc);
-% %         end
-%         [ J,dJdx,dJdy ] = computeCost( pointsTrue,ccalc,rHat );
-%     end
-% %     fprintf('abs(J-Jprev)/J is %f\n',abs(J-Jprev)/J);
-%     if(abs(J-Jprev)/J)<epsouter
-%         break
-%     end
-%     Jprev = J;
-%     i = i+1;
-
-% end
-
-% %% plot results
-% if viz
-%     plot(ccalc.x,ccalc.y,'.g','MarkerSize',20)
-%     rectangle('Position',[ccalc.x-rHat ccalc.y-rHat 2*rHat 2*rHat],'Curvature',[1 1],'EdgeColor','g')
-% end
-
 
--- Probes/classification/generateRandomPtsModified.m
+++ Probes/classification/generateRandomPtsModified.m
@@ -1,63 +1,77 @@
-function [ points ] = generateRandomPts( r,cx,cy,npt,aoa,noise,target )
+function [ pointsTrue, pointsFalse ] = generateRandomPts( r,cx,cy,npt,aoa,noise,target )
 
-viz = false;
+    visualize = false;
 
-angles = linspace(-aoa/2,aoa/2,npt)';
-
-% generate vector
-if (viz)
-    figure
-    plot(0,0,'x')
-    hold on; axis equal; grid on;
-    rectangle('Position',[cx-r cy-r 2*r 2*r],'Curvature',[1 1],'EdgeColor','black')
-end
-
-if target
-    for i = 1:npt
-
-        line = -r*noise:r/50:r*noise;
-        line = line + r;
-
-        vector.x = cx+line*sind(angles(i));
-        vector.y = cy+line*cosd(angles(i));
-
-        if (viz)
-            plot(vector.x,vector.y)
-        end
-
-        randIndex = randi([1 length(line)]);
-
-        points.x(i) = vector.x(randIndex);
-        points.y(i) = vector.y(randIndex);
-        points.target(i) = true;
-
+    if (visualize)
+        figure
+        plot(0,0,'x')
+        hold on; axis equal; grid on;
+        rectangle('Position',[cx-r cy-r 2*r 2*r],'Curvature',[1 1],'EdgeColor','black')
     end
-else
-    for i = 1:npt
+    
+    angles = linspace(-aoa/2,aoa/2,npt)'; % get probe angles
 
-        line = -r*noise*5:r/50:r*noise*5;
-        line = line + r;
+    if target
+        for i = 1:npt
 
-        vector.x = cx+line*sind(angles(i));
-        vector.y = cy+line*cosd(angles(i));
+            line = -r*noise:r/50:r*noise;
+            line = line + r;
 
-        if (viz)
-            plot(vector.x,vector.y)
+            vector.x = cx+line*sind(angles(i));
+            vector.y = cy+line*cosd(angles(i));
+
+            if (visualize)
+                plot(vector.x,vector.y)
+            end
+
+            randIndex = randi([1 length(line)]);
+
+            points.x(i) = vector.x(randIndex);
+            points.y(i) = vector.y(randIndex);
+            points.target(i) = true;
+
         end
+    else
+        for i = 1:npt
 
-        randIndex = randi([1 length(line)]);
+            line = -r*noise*5:r/50:r*noise*5;
+            line = line + r;
 
-        points.x(i) = vector.x(randIndex);
-        points.y(i) = vector.y(randIndex);
-        points.target(i) = randi([0 1]);
+            vector.x = cx+line*sind(angles(i));
+            vector.y = cy+line*cosd(angles(i));
 
+            if (visualize)
+                plot(vector.x,vector.y)
+            end
+
+            randIndex = randi([1 length(line)]);
+
+            points.x(i) = vector.x(randIndex);
+            points.y(i) = vector.y(randIndex);
+            points.target(i) = randi([0 1]);
+
+        end
     end
-end
 
-if (viz)
-    plot(points.x,points.y)
-    hold off
-end
+    % separate data
+    trueCounter = 1; falseCounter = 1;
+    pointsTrue = []; pointsFalse = [];
+    for i = 1:length(points.target)
+        if points.target(i)
+           pointsTrue.x(trueCounter) = points.x(i);
+           pointsTrue.y(trueCounter) = points.y(i);
+           trueCounter = trueCounter + 1;
+        else
+           pointsFalse.x(trueCounter) = points.x(i);
+           pointsFalse.y(trueCounter) = points.y(i);
+           falseCounter = falseCounter + 1; 
+        end
+    end
+
+    if (visualize)
+        plot(points.x,points.y)
+        hold off
+    end
 
 end
 
